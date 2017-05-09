e4da0515f36779123336c3cd00e9b1623e704c7d
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-05-09 10:04:10 +0000

    Added new, edit and show view for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,53 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-    def new
-        
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params) 
+    @article.user = current_user
+    if @article.save
+      flash[:success] = "Your article has been posted!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
     end
-    
-    def create
-        
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @article.update(article_params)
+      flash[:success] = "Your article has been updated!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
-    
-    def edit
-        
-    end
-    
-    def update
-        
-    end
-    
-    def delete
-        
-    end
-    
+  end
+  
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+  end
+  
+  def destroy
+    @article.destroy
+    flash[:danger] = "This article has been removed"
+    redirect_to articles_path
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :description, :user_id)
+  end
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
