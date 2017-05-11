4304cdd2565fd25a42baba8366ac1240bc046166
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-11 11:07:14 +0000

    Created New action in categories, with corresponding view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,22 @@
 class CategoriesController < ApplicationController
     
     def index
-        
+        @categories = Category.all
     end
 
     def new
-        
+        @category = Category.new
     end
     
     def create
-        
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = "Category successfully saved"
+            redirect_to categories_path
+        else
+            flash[:danger] = "Category unable to be saved, contact your admin"
+            render 'new'
+        end
     end
     
     def edit
@@ -26,6 +33,11 @@ class CategoriesController < ApplicationController
     
     def destroy
         
+    end
+    
+    private
+    def category_params
+        params.require(:category).permit(:name)
     end
     
 end
