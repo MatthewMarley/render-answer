47bd5876116db3e5c61d9f3d150baa522bbad538
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-11 11:26:11 +0000

    Added validations to Categories, and associations to articles

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
-    
-    
+    has_many :articles_categories
+    has_many :articles, through: :article_categories
+    validates :name, presence: true, length: { maximum: 50 }
+    validates_uniqueness_of :name
 end
