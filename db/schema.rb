b4b4bcdd0a474766f9005c7e8ba74aa754b48064
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-11 10:35:22 +0000

    Forgot to branch off and created category migration in master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507125914) do
+ActiveRecord::Schema.define(version: 20170511103145) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20170507125914) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
