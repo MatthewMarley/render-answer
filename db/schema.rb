feb5a4af2c0870056b679e19726a327eefd64a4e
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-11 10:51:46 +0000

    Added category MVC and migration for articlecategories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511103145) do
+ActiveRecord::Schema.define(version: 20170511103736) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id"
