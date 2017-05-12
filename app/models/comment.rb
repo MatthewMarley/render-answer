8c6401e1945550769cd390546f726fbdd9bfc481
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-05-12 11:38:02 +0000

    Added delete comment, comment pagination, require_user to article and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
   
-  validates :comment, presence: true, length: { minimum: 5, maximum: 300 }
+  validates :body, presence: true, length: { minimum: 5, maximum: 300 }
   
 end
